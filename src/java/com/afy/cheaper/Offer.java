1fb44b9020e14df2cbc7323f2c23e74d7ea40d34
Inrahim Sayed <ibrahim.sayed7@gmail.com>
2017-11-06 15:06:42 +0200

    first code

---
+++ src/java/com/afy/cheaper/Offer.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.afy.cheaper;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author isayed
+ */
+@Entity
+@Table(name = "offer")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Offer.findAll", query = "SELECT o FROM Offer o")
+    , @NamedQuery(name = "Offer.findById", query = "SELECT o FROM Offer o WHERE o.id = :id")
+    , @NamedQuery(name = "Offer.findByName", query = "SELECT o FROM Offer o WHERE o.name = :name")
+    , @NamedQuery(name = "Offer.findByStatus", query = "SELECT o FROM Offer o WHERE o.status = :status")
+    , @NamedQuery(name = "Offer.findByDescription", query = "SELECT o FROM Offer o WHERE o.description = :description")})
+public class Offer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 45)
+    @Column(name = "name")
+    private String name;
+    @Size(max = 45)
+    @Column(name = "status")
+    private String status;
+    @Size(max = 45)
+    @Column(name = "description")
+    private String description;
+
+    public Offer() {
+    }
+
+    public Offer(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Offer)) {
+            return false;
+        }
+        Offer other = (Offer) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.afy.cheaper.Offer[ id=" + id + " ]";
+    }
+    
+}
